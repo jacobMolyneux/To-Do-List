6cfd46b49dbe304369e5eb0545ee5d1c4f8af79b
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-05-24 02:09:41 -0400

    added a new module to create new events

--- src/listObject.js
+++ src/listObject.js
@@ -1,8 +1,30 @@
-const item = (name, date, time, details) => {
+class item{
+constructor(name, date, time, details){
     this.name = name;
     this.date = date;
     this.time = time;
     this.details = details;
-
+    
+}}
+function createElementFromObject(item, parent){
+    const itemContainer = document.createElement('div');
+    itemContainer.classList = 'item';
+    const titleDisplay = document.createElement('div');
+    titleDisplay.textContent = item.name;
+    titleDisplay.setAttribute('id', 'name'); 
+    const dateDisplay = document.createElement('div');
+    dateDisplay.textContent = item.date;
+    dateDisplay.setAttribute('id', 'date');
+    const timeDisplay = document.createElement('div');
+    timeDisplay.textContent = item.time;
+    timeDisplay.setAttribute('id', 'time'); 
+    const descriptionDisplay = document.createElement('div');
+    descriptionDisplay.textContent = item.details;
+    descriptionDisplay.setAttribute('id', 'details'); 
+    itemContainer.appendChild(titleDisplay); 
+    itemContainer.appendChild(dateDisplay);
+    itemContainer.appendChild(timeDisplay);
+    itemContainer.appendChild(descriptionDisplay); 
+    parent.appendChild(itemContainer);
 }
-export default item
+export { item, createElementFromObject}
